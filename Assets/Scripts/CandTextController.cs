4219916ca88e86db55b321d8a37f1338d932a671
Idanord <33676867+Idanord@users.noreply.github.com>
2017-12-02 19:09:34 -0800

    Finished Text
    Finished implementing the text. Need to go over the text again to replace the edits.

--- Assets/Scripts/CandTextController.cs
+++ Assets/Scripts/CandTextController.cs
@@ -10,7 +10,7 @@ public class CandTextController : MonoBehaviour {
     public Text gameText;
 
     //Private variables for states
-    private enum States { candy1, candy2, tryAgain, candy3, tryAgain2, candy4 };
+    private enum States { candy1, candy2, tryAgain, candy3, tryAgain2, candy4, leavingCandy, leavingCandy2, tryAgain3, tryAgain4, homeless, talkToHomeless };
     private States myState;
 
 	// Use this for initialization
@@ -43,6 +43,30 @@ public class CandTextController : MonoBehaviour {
         else if(myState == States.candy4)
         {
             state_candy4();
+        }
+        else if (myState == States.leavingCandy)
+        {
+            state_leavingCandy();
+        }
+        else if (myState == States.leavingCandy2)
+        {
+            state_leavingCandy2();
+        }
+        else if (myState == States.homeless)
+        {
+            state_homeless();
+        }
+        else if (myState == States.tryAgain3)
+        {
+            state_tryAgain3();
+        }
+        else if (myState == States.tryAgain4)
+        {
+            state_tryAgain4();
+        }
+        else if (myState == States.talkToHomeless)
+        {
+            state_talkToHomeless();
         }
 	}
 
@@ -117,9 +141,86 @@ public class CandTextController : MonoBehaviour {
                         "Press the Right Arrow key to advance.";
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
+            myState = States.leavingCandy;
+        }
+    }
+
+    void state_leavingCandy()
+    {
+        gameText.text = "Rocket: Yummmmm! They have my favorite soda: Strawberry Shortcake!" +
+                        "I’m going to ask my mom to get me some as a special treat some day soon!\n\n" +
+                        "Press the Right Arrow key to advance";
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            myState = States.leavingCandy2;
+        }
+    }
+
+    void state_leavingCandy2()
+    {
+        gameText.text = "You pass a homeless person on the street with a sign that says “Homeless. Please help. Thank you. God bless.”\n\n" +
+                        "Press the Right Arrow key to advance";
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            myState = States.homeless;
+        }
+    }
+
+    void state_homeless()
+    {
+        gameText.text = "Rocket: Hello there.\n\n" +
+                        "Do you Ignore the homeless person entirely and keep walking?\n (Press the I key)\n\n" +
+                        "Do you smile and nod along with Rocket but Keep walking?\n (Press the K key)\n\n" +
+                        "Do you take a moment to remember if you have any spare Change and ask Rocket to spare some change as well?\n (Press the C key)";
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            myState = States.tryAgain3;
+        } else if (Input.GetKeyDown(KeyCode.K))
+        {
+            myState = States.tryAgain4;
+        } else if (Input.GetKeyDown(KeyCode.C))
+        {
+            myState = States.talkToHomeless;
+        }
+    }
+
+    void state_tryAgain3()
+    {
+        gameText.text = "Rocket: Hello there.\n\n" +
+                       "Do you smile and nod along with Rocket but Keep walking?\n (Press the K key)\n\n" +
+                       "Do you take a moment to remember if you have any spare Change and ask Rocket to spare some change as well?\n (Press the C key)";
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            myState = States.tryAgain4;
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            myState = States.talkToHomeless;
+        }
+    }
+
+    void state_tryAgain4()
+    {
+        gameText.text = "Rocket: Hello there.\n\n" +
+                        "Do you take a moment to remember if you have any spare Change and ask Rocket to spare some change as well?\n (Press the C key)";
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            myState = States.talkToHomeless;
+        }
+    }
+
+    void state_talkToHomeless()
+    {
+        gameText.text = "Rocket: I would love to help. Here, I have a few quarters, as do you." +
+                        "That’s $2. Rocket hands the money to the person. The person says thank you!" +
+                        "Thanks for helping that person. It’s always good to offer kindness to strangers when and how you can.\n\n" +
+                        "Press the Right Arrow key to advance";
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
             SceneManager.LoadScene("UrbanJungleStreet");
         }
     }
+
 }//end of script
 
 //Will Fisher
--- Assets/Scripts/NeedleTextController.cs
+++ Assets/Scripts/NeedleTextController.cs
@@ -10,7 +10,7 @@ public class NeedleTextController : MonoBehaviour {
     public Text gameText;
 
     //private variables for enum
-    private enum States { needle1, needle2, tryAgain, needle3, tryAgain2, needle4 };
+    private enum States { needle1, needle2, tryAgain, needle3, tryAgain2, needle4, walking1, walking2, homeless, tryAgain3, tryAgain4, talkToHomeless };
     private States myState;
 
 	// Use this for initialization
@@ -20,7 +20,7 @@ public class NeedleTextController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(myState == States.needle1)
+        if (myState == States.needle1)
         {
             state_needle1();
         }
@@ -32,19 +32,43 @@ public class NeedleTextController : MonoBehaviour {
         {
             state_tryAgain();
         }
-        else if(myState == States.needle3)
+        else if (myState == States.needle3)
         {
             state_needle3();
         }
-        else if(myState == States.tryAgain2)
+        else if (myState == States.tryAgain2)
         {
             state_tryAgain2();
         }
-        else if(myState == States.needle4)
+        else if (myState == States.needle4)
         {
             state_needle4();
         }
-	}
+        else if (myState == States.walking1)
+        {
+            state_walking1();
+        }
+        else if (myState == States.walking2)
+        {
+            state_walking2();
+        }
+        else if (myState == States.homeless)
+        {
+            state_homeless();
+        }
+        else if (myState == States.tryAgain3)
+        {
+            state_tryAgain3();
+        }
+        else if (myState == States.tryAgain4)
+        {
+            state_tryAgain4();
+        }
+        else if (myState == States.talkToHomeless)
+        {
+            state_talkToHomeless();
+        }
+    }
 
     void state_needle1()
     {
@@ -116,6 +140,82 @@ public class NeedleTextController : MonoBehaviour {
     {
         gameText.text = "Rocket:  They were so grateful we let them know about the sharp needles (sharps) out in front." +
                         "Now they can carefully clean them up or let the city authorities know. Good work!\n\n" +
+                        "Press the Right Arrow key to advance";
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            myState = States.walking1;
+        }
+    }
+
+    void state_walking1()
+    {
+        gameText.text = "Rocket: Yummmmm! And at that store, Rocket Fizz, they have my favorite soda: Strawberry Shortcake!" +
+                        "I’m going to ask my mom to get me some as a special treat some day soon!\n\n" +
+                        "Press the Right Arrow key to advance";
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            myState = States.walking2;
+        }
+    }
+
+    void state_walking2()
+    {
+        gameText.text = "You pass a homeless person on the street with a sign that says “Homeless. Please help. Thank you. God bless.”\n\n" +
+                        "Press the Right Arrow key to advance";
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            myState = States.homeless;
+        }
+    }
+
+    void state_homeless()
+    {
+        gameText.text = "Rocket: Hello there.\n\n" +
+                        "Do you Ignore the homeless person entirely and keep walking?\n (Press the I key)\n\n" +
+                        "Do you smile and nod along with Rocket but Keep walking?\n (Press the K key)\n\n" +
+                        "Do you take a moment to remember if you have any spare Change and ask Rocket to spare some change as well?\n (Press the C key)";
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            myState = States.tryAgain3;
+        } else if (Input.GetKeyDown(KeyCode.K))
+        {
+            myState = States.tryAgain4;
+        } else if (Input.GetKeyDown(KeyCode.C))
+        {
+            myState = States.talkToHomeless;
+        }
+    }
+
+    void state_tryAgain3()
+    {
+        gameText.text = "Rocket: Hello there.\n\n" +
+                        "Do you smile and nod along with Rocket but Keep walking?\n (Press the K key)\n\n" +
+                        "Do you take a moment to remember if you have any spare Change and ask Rocket to spare some change as well?\n (Press the C key)";
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            myState = States.tryAgain4;
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            myState = States.talkToHomeless;
+        }
+    }
+
+    void state_tryAgain4()
+    {
+        gameText.text = "Rocket: Hello there.\n\n" +
+                        "Do you take a moment to remember if you have any spare Change and ask Rocket to spare some change as well?\n (Press the C key)";
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            myState = States.talkToHomeless;
+        }
+    }
+
+    void state_talkToHomeless()
+    {
+        gameText.text = "Rocket: I would love to help. Here, I have a few quarters, as do you." +
+                        "That’s $2. Rocket hands the money to the person. The person says thank you!" +
+                        "Thanks for helping that person. It’s always good to offer kindness to strangers when and how you can.\n\n" +
                         "Press the Right Arrow key to advance";
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
